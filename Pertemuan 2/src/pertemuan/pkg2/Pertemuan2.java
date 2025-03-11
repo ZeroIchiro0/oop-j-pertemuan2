19535282f1969a03a9f873c69bd50ecc9d423f8f
ZeroIchiro0 <aurora.1412.4869@gmail.com>
2025-03-11 21:55:28 +0800

    Pertemuan 2

---
+++ Pertemuan 2/src/pertemuan/pkg2/Pertemuan2.java
@@ -0,0 +1,24 @@
+package pertemuan.pkg2;
+
+/**
+ *
+ * @author Gabriel Friskila
+ * Date: Selasa, 11 Maret 2025
+ */
+public class Pertemuan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama = "ZeroIchiro";
+        String nim = "2301010429";
+        
+        System.out.println("Pertemuan 2 Java"); //println tulisan selanjutnya berada dibawahnya
+        System.out.println("----------------");
+        System.out.println("Nama    : "+nama); //jika "+" distring berarti disambung
+        System.out.println("NIM     : "+nim);
+        System.out.printf("Nama saya %s dengan Nim %s, Saat ini sedang tidur",nama,nim); //Jika berupa tulisan (string) menggunakan "%s"
+    }
+    
+}
